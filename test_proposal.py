2856b05e653f10d6753f7eb10a5125aa6861deac
Adam Peter Banham <n7176546@qut.edu.au>
2023-07-23 20:46:56 +1000

    updated testing to reflect guard precision.

--- test_proposal.py
+++ test_proposal.py
@@ -28,7 +28,7 @@ AX_6_LOGS = [
 AX_7_FOLD = join(AXS_FOLD, "axiom 7")
 AX_7_LOG = join(AX_7_FOLD, "log_1.xes")
 AX_7_MODELS = [
-    join(AX_7_FOLD, f"ax7_model_{i}.pnml")
+    join(AX_7_FOLD, f"ax7_model_{i}b.pnml")
     for i 
     in range(1,6)
 ]
@@ -47,7 +47,7 @@ AX_9_LOGS = [
     in range(1,4)
 ]
 
-AX_RERUNS = 10
+AX_RERUNS = 11
 
 def axiom_1():
     print("testing axiom 1 for unpublished measurement (grec_E).")
@@ -139,56 +139,114 @@ def axiom_6():
           " must contain the same value for each step")
     info(f"outcome (mean,std) :: {mean_computes}")
 
-
+@enable_logging
 def axiom_7():
-    print("testing axiom 7 for unpublished measurement (gprec_F).")
+    info("testing axiom 7 for unpublished measurement (gprec_F).")
+    mean_runtimes = []
+    mean_computes = []
+    log = read_xes_complex(AX_7_LOG)
     for test_no,model_file in enumerate(AX_7_MODELS):
         results = []
+        runtimes = []
         for run in range(1,AX_RERUNS):
-            print(f"computing run {run}...")
-            res = reasoning_precision(AX_7_LOG, model_file)
+            info(f"computing run {run}...")
+            stime = time()
+            res = compute_guard_precision(log, parse_pnml_for_dpn(model_file),
+                                          optimised=True)
+            runtimes.append(time() - stime)
             results.append(res)
         mean = sum(results) / len(results)
         std = [ (res - mean) ** 2 for res in results ]
         std = sum(std) / len(std)
         std = sqrt(std)
-        print(f"results for model {test_no+1} of axiom 7 are :: {mean=} | {std=}.")
-        print(f"unique results observed :: {results}.")
+        info(f"results for model {test_no+1} of axiom 7 are :: {mean=} | {std=}.")
+        info(f"unique results observed :: {results}.")
+        runtime = sum(runtimes) / len(runtimes)
+        info(f"mean compute time between runs :: {runtime:.1f} secs")
+        mean_runtimes.append(runtime)
+        mean_computes.append((mean, std))
+    info(f"average runtimes for tests : {mean_runtimes}")
+    info("testing completed for axiom seven, to adhere the following series" + 
+          " must be strictly increasing from right to left")
+    info(f"outcome (mean,std) :: {mean_computes}")
 
+
+@enable_logging
 def axiom_8():
-    print("testing axiom 8 for unpublished measurement (gprec_F).")
+    info("testing axiom 8 for unpublished measurement (gprec_F).")
+    log = read_xes_complex(AX_8_LOG)
+    mean_computes = []
+    mean_runtimes = []
     for test_no,model_file in enumerate(AX_8_MODELS):
         results = []
+        runtimes = []
         for run in range(1,AX_RERUNS):
-            print(f"computing run {run}...")
-            res = reasoning_precision(AX_8_LOG, model_file)
+            info(f"computing run {run}...")
+            stime = time()
+            res = compute_guard_precision(log, parse_pnml_for_dpn(model_file),
+                                          optimised=True)
+            runtimes.append(time() - stime)
             results.append(res)
         mean = sum(results) / len(results)
         std = [ (res - mean) ** 2 for res in results ]
         std = sum(std) / len(std)
         std = sqrt(std)
-        print(f"results for model {test_no+1} of axiom 8 are :: {mean=} | {std=}.")
-        print(f"unique results observed :: {results}.")
+        info(f"results for model {test_no+1} of axiom 8 are :: {mean=} | {std=}.")
+        info(f"unique results observed :: {results}.")
+        runtime = sum(runtimes) / len(runtimes)
+        info(f"mean compute time between runs :: {runtime:.1f} secs")
+        mean_runtimes.append(runtime)
+        mean_computes.append((mean, std))
+    info(f"average runtimes for tests : {mean_runtimes}")
+    info("testing completed for axiom eight, to adhere the following series" + 
+          " must only return the max of the measure (1.0)")
+    info(f"outcome (mean,std) :: {mean_computes}")
 
+@enable_logging
 def axiom_9():
-    print("testing axiom 9 for unpublished measurement (gprec_F).")
+    info("testing axiom 9 for unpublished measurement (gprec_F).")
+    mean_computes = []
+    mean_runtimes = []
     for test_no, logfile in enumerate(AX_9_LOGS):
         results = []
+        runtimes = []
         for run in range(1,AX_RERUNS):
-            print(f"computing run {run}...")
-            res = reasoning_precision(logfile, AX_9_MODEL)
+            log = read_xes_complex(logfile)
+            info(f"computing run {run}...")
+            stime = time()
+            res = compute_guard_precision(log, parse_pnml_for_dpn(AX_9_MODEL),
+                                          optimised=True)
+            runtimes.append(time() - stime)
             results.append(res)
         mean = sum(results) / len(results)
         std = [ (res - mean) ** 2 for res in results ]
         std = sum(std) / len(std)
         std = sqrt(std)
-        print(f"results for log {test_no+1} of axiom 9 are :: {mean=} | {std=}.")
-        print(f"unique results observed :: {set(results)}.")
+        info(f"results for log {test_no+1} of axiom 9 are :: {mean=} | {std=}.")
+        info(f"unique results observed :: {set(results)}.")
+        runtime = sum(runtimes) / len(runtimes)
+        info(f"mean compute time between runs :: {runtime:.1f} secs")
+        mean_computes.append((mean,std))
+        mean_runtimes.append(runtime)
+    info(f"average runtimes for tests : {mean_runtimes}")
+    info("testing completed for axiom nine, to adhere the following series" + 
+        " must contain the same value for each step")
+    info(f"outcome (mean,std) :: {mean_computes}")
+
+@enable_logging
+def expression_test():
+    log = read_xes_complex(AX_8_LOG)
+    model = parse_pnml_for_dpn(AX_8_MODELS[-1])
+    print(model)
+    prec = compute_guard_precision(log, model, optimised=True)
+    info(prec)
 
 if __name__ == "__main__":
-    axiom_3(debug=True)
-    axiom_4(debug=True)
-    axiom_6(debug=True)
-    # axiom_7()
-    # axiom_8()
-    # axiom_9()
+    # guard-recall testing
+    # axiom_3(debug=True)
+    # axiom_4(debug=True)
+    # axiom_6(debug=True)
+    # guard-precision testing
+    axiom_7(debug=True)
+    axiom_8(debug=True)
+    axiom_9(debug=True)
