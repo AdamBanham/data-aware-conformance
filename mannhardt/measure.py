bdbe7f920ee1a266179f5975108664106db66a27
Adam Peter Banham <n7176546@qut.edu.au>
2023-10-20 17:43:49 +1000

    added testing for prom executables against counter-examples

---
+++ mannhardt/measure.py
@@ -0,0 +1,90 @@
+from os.path import join 
+from subprocess import run
+from typing import Dict
+
+from tempfile import TemporaryDirectory
+
+# note that the bat file is only suitable for windows systems
+# but an alternative is avaiable in the same folder for linux.
+JAR_EXE = join(".","java", "build", "install", "PromExecutables", "bin", 
+               "PromExecutables.bat")
+
+def test_for_executables() -> bool:
+    """
+    Checks for the executables needed to run the technique from ProM.
+    """
+    import os 
+    if not os.path.exists(JAR_EXE):
+        return False 
+    return True
+
+def handle_output(tmpdir):
+    """
+    A quick shortcut to process the measurement from the run of the java
+    executable.
+    """
+    with open(join(tmpdir, "scores.csv")) as f:
+        columns = f.readline().split(",")
+        data = f.readline().split(",")
+        return dict(
+            (key,val)
+            for key,val 
+            in zip(columns,data)
+        )
+
+def call_java(log, model, type) -> Dict[str,str]:
+    """
+    Calls the java executable with the parameters need to trigger de Leoni,
+    returns a dict of measurements for the java call.
+    """
+    import sys
+    with TemporaryDirectory() as tmpdir:
+        out = run([ 
+            JAR_EXE,
+            "--job",
+            type,
+            "--model",
+            model,
+            "--log",
+            log,
+            "--output",
+            tmpdir
+        ],
+        stdout=sys.stdout)
+        print(f"returned value : {out}")
+        processed = handle_output(tmpdir)
+        print(f"{processed}")
+        print(f"outcome :: {processed['mean']}")
+        return processed
+        
+def guard_recall(logfile:str, modelfile:str) -> float:
+    """
+    Computes the recall notion from "Balanced multi-perspective checking of 
+    process conformance" by F. Mannhardt et. al. in 2015.
+
+    However, PromExecutables (see java dir) needs to be compiled before
+    this function can return a value.
+    """
+    # check for executables
+    if not test_for_executables():
+        raise Exception(
+            "Unable to compute recall notion as PromExecutables"+
+            "have not been compiled, see java directory to compile.")
+    run_data = call_java(logfile,modelfile, "MannhardtRecall")
+    return float(run_data["mean"])
+
+def guard_precision(logfile:str, modelfile:str) -> float:
+    """
+    Computes the precision notion from "Measuring the Precision of Multi-perspective
+    Process Models" by F. Mannhardt et. al. in 2016.
+
+    However, PromExecutables (see java dir) needs to be compiled before
+    this function can return a value.
+    """
+    # check for executables
+    if not test_for_executables():
+        raise Exception(
+            "Unable to compute recall notion as PromExecutables"+
+            "have not been compiled, see java directory to compile.")
+    run_data = call_java(logfile,modelfile, "MannhardtPrecision")
+    return float(run_data["mean"])
---
+++ test_mannhardt.py
@@ -0,0 +1,272 @@
+from pmkoalas._logging import info, enable_logging
+from mannhardt.measure import guard_recall,guard_precision
+
+from time import time
+from os.path import join 
+from math import sqrt
+
+AXS_FOLD = join(".", "axioms")
+AX_3_FOLD = join(AXS_FOLD, "axiom 3")
+AX_3_LOG = join(AX_3_FOLD, "log_1.xes")
+AX_3_MODELS = [ 
+    join(AX_3_FOLD, f"ax3_model_{i}.pnml")
+    for i 
+    in range(1,7)
+]
+AX_4_FOLD = join(AXS_FOLD, "axiom 4")
+AX_4_LOG = join(AX_4_FOLD, "log_1.xes")
+AX_4_MODEL = join(AX_4_FOLD, "ax4_model_1.pnml")
+AX_6_FOLD = join(AXS_FOLD, "axiom 6")
+AX_6_MODEL = join(AX_6_FOLD, "ax6_model_1.pnml")
+AX_6_LOGS = [ 
+    join(AX_6_FOLD, f"log_{i}.xes")
+    for i 
+    in range(1,4)
+]
+AX_7_FOLD = join(AXS_FOLD, "axiom 7")
+AX_7_LOG = join(AX_7_FOLD, "log_1.xes")
+AX_7_MODELS = [
+    join(AX_7_FOLD, f"ax7_model_{i}b.pnml")
+    for i 
+    in range(1,6)
+]
+AX_8_FOLD = join(AXS_FOLD, "axiom 8")
+AX_8_LOG = join(AX_8_FOLD, "log_1.xes")
+AX_8_MODELS = [ 
+    join(AX_8_FOLD, f"ax8_model_{i}.pnml")
+    for i
+    in range(1,5)
+]
+AX_8_NEG_MODELS = [ 
+    join(AX_8_FOLD, f"ax8_model_{i}.pnml")
+    for i
+    in range(5,6)
+]
+AX_9_FOLD = join(AXS_FOLD, "axiom 9")
+AX_9_MODEL = join(AX_9_FOLD, "ax9_model_1.pnml")
+AX_9_LOGS = [ 
+    join(AX_9_FOLD, f"log_{i}.xes")
+    for i 
+    in range(1,4)
+]
+
+AX_RERUNS = 2
+OPTIMISED_RUN = True
+
+@enable_logging
+def axiom_3():
+    info("testing axiom 3 for proposal of guard-recall.")
+    log = AX_3_LOG
+    mean_computes = []
+    mean_runtimes = []
+    for test_no,model_file in enumerate(AX_3_MODELS):
+        results = []
+        ctimes = []
+        for run in range(1,AX_RERUNS):
+            info(f"computing run {run}...")
+            stime = time()
+            res = guard_recall(log,model_file)
+            ctimes.append(time() - stime)
+            results.append(res)
+        mean = sum(results) / len(results)
+        std = [ (res - mean) ** 2 for res in results ]
+        std = sum(std) / len(std)
+        std = sqrt(std)
+        info(f"results for model {test_no+1} of axiom 3 are :: {mean=} | {std=}.")
+        info(f"unique results observed :: {set(results)}.")
+        runtime = sum(ctimes)/ len (ctimes)
+        mean_runtimes.append(f"{runtime:.1f} seconds")
+        info(f"average compute time for runs :: {runtime:.1f} seconds.")
+        mean_computes.append((mean, std))
+    info(f"average runtimes for tests : {mean_runtimes}")
+    info("testing completed for axiom three, to adhere the following series" + 
+          " must be strictly increasing from left to right")
+    info(f"outcome (mean,std) :: {mean_computes}")
+
+@enable_logging
+def axiom_4():
+    info("testing axiom 4 for proposal of guard-recall.")
+    results = []
+    log = AX_4_LOG
+    model = AX_4_MODEL
+    runtimes = []
+    for run in range(1,AX_RERUNS):
+        info(f"computing run {run}...")
+        stime = time()
+        res = guard_recall(log,model)
+        runtimes.append(time() - stime)
+        results.append(res)
+    mean = sum(results) / len(results)
+    std = [ (res - mean) ** 2 for res in results ]
+    std = sum(std) / len(std)
+    std = sqrt(std)
+    info(f"results of testing axiom 4 are :: {mean=} | {std=}.")
+    info(f"unique results observed :: {set(results)}.") 
+    runtimes = sum(runtimes)/len(runtimes)
+    info(f"average  compute time for runs :: {runtimes:.1f} seconds.")
+    info("testing completed for axiom four, to adhere the measure must " +
+         "return zero.")
+    info(f"outcomes :: {set(results)}")
+
+@enable_logging
+def axiom_6():
+    info("testing axiom 6 for proposal of guard-recall.")
+    model = AX_6_MODEL
+    mean_computes = []
+    mean_runtimes = []
+    for test_no, logfile in enumerate(AX_6_LOGS):
+        results = []
+        runtimes = []
+        for run in range(1,AX_RERUNS):
+            info(f"computing run {run}...")
+            stime = time()
+            res = guard_recall(logfile,model)
+            runtimes.append(time() - stime)
+            results.append(res)
+        mean = sum(results) / len(results)
+        std = [ (res - mean) ** 2 for res in results ]
+        std = sum(std) / len(std)
+        std = sqrt(std)
+        runtimes = sum(runtimes) / len(runtimes)
+        info(f"results for log {test_no+1} of axiom 6 are :: {mean=} | {std=}.")
+        info(f"unique results observed :: {set(results)}.")
+        info(f"mean compute time between runs :: {runtimes:.1f} seconds")
+        mean_runtimes.append(f"{runtimes:.1f}")
+        mean_computes.append((mean,std))
+    info(f"average runtimes for tests : {mean_runtimes}")
+    info("testing completed for axiom six, to adhere the following series" + 
+          " must contain the same value for each step")
+    info(f"outcome (mean,std) :: {mean_computes}")
+
+@enable_logging
+def axiom_7():
+    info("testing axiom 7 for unpublished measurement (gprec_F).")
+    mean_runtimes = []
+    mean_computes = []
+    log = AX_7_LOG
+    for test_no,model_file in enumerate(AX_7_MODELS):
+        results = []
+        runtimes = []
+        for run in range(1,AX_RERUNS):
+            info(f"computing run {run}...")
+            stime = time()
+            res = guard_precision(log, model_file)
+            runtimes.append(time() - stime)
+            results.append(res)
+        mean = sum(results) / len(results)
+        std = [ (res - mean) ** 2 for res in results ]
+        std = sum(std) / len(std)
+        std = sqrt(std)
+        info(f"results for model {test_no+1} of axiom 7 are :: {mean=} | {std=}.")
+        info(f"unique results observed :: {results}.")
+        runtime = sum(runtimes) / len(runtimes)
+        info(f"mean compute time between runs :: {runtime:.1f} secs")
+        mean_runtimes.append(runtime)
+        mean_computes.append((mean, std))
+    info(f"average runtimes for tests : {mean_runtimes}")
+    info("testing completed for axiom seven, to adhere the following series" + 
+          " must be strictly increasing from right to left")
+    info(f"outcome (mean,std) :: {mean_computes}")
+
+
+@enable_logging
+def axiom_8():
+    info("testing axiom 8 for unpublished measurement (gprec_F).")
+    log = AX_8_LOG
+    mean_computes = []
+    mean_runtimes = []
+    mean_neg_computes = []
+    mean_neg_runtimes = []
+    # should be 1.0
+    for test_no,model_file in enumerate(AX_8_MODELS):
+        results = []
+        runtimes = []
+        for run in range(1,AX_RERUNS):
+            info(f"computing run {run}...")
+            stime = time()
+            res = None
+            try:
+                res = guard_precision(log, model_file)
+            except Exception as e:
+                res = -1
+                info(f"Failed to compute a precision measure for model {test_no}")
+            runtimes.append(time() - stime)
+            results.append(res)
+        mean = sum(results) / len(results)
+        std = [ (res - mean) ** 2 for res in results ]
+        std = sum(std) / len(std)
+        std = sqrt(std)
+        info(f"results for model {test_no+1} of axiom 8 are :: {mean=} | {std=}.")
+        info(f"unique results observed :: {results}.")
+        runtime = sum(runtimes) / len(runtimes)
+        info(f"mean compute time between runs :: {runtime:.1f} secs")
+        mean_runtimes.append(runtime)
+        mean_computes.append((mean, std))
+    # shouldn't be 1.0
+    for test_no,model_file in enumerate(AX_8_NEG_MODELS):
+        results = []
+        runtimes = []
+        for run in range(1,AX_RERUNS):
+            info(f"computing run {run}...")
+            stime = time()
+            res = guard_precision(log, model_file)
+            runtimes.append(time() - stime)
+            results.append(res)
+        mean = sum(results) / len(results)
+        std = [ (res - mean) ** 2 for res in results ]
+        std = sum(std) / len(std)
+        std = sqrt(std)
+        info(f"results for model {test_no+1} of axiom 8 are :: {mean=} | {std=}.")
+        info(f"unique results observed :: {results}.")
+        runtime = sum(runtimes) / len(runtimes)
+        info(f"mean compute time between runs :: {runtime:.1f} secs")
+        mean_neg_runtimes.append(runtime)
+        mean_neg_computes.append((mean, std))
+    info(f"average runtimes for tests : {mean_runtimes+mean_neg_runtimes}")
+    info("testing completed for axiom eight, to adhere the following series" + 
+          " must only return the max of the measure (1.0)")
+    info(f"outcome (mean,std) :: {mean_computes}")
+    info("Furthermore, to adhere the following series" + 
+          " must not return the max of the measure (1.0)")
+    info(f"outcome (mean,std) :: {mean_neg_computes}")
+
+@enable_logging
+def axiom_9():
+    info("testing axiom 9 for unpublished measurement (gprec_F).")
+    mean_computes = []
+    mean_runtimes = []
+    for test_no, logfile in enumerate(AX_9_LOGS):
+        results = []
+        runtimes = []
+        for run in range(1,AX_RERUNS):
+            log = logfile
+            info(f"computing run {run}...")
+            stime = time()
+            res = guard_precision(log, AX_9_MODEL)
+            runtimes.append(time() - stime)
+            results.append(res)
+        mean = sum(results) / len(results)
+        std = [ (res - mean) ** 2 for res in results ]
+        std = sum(std) / len(std)
+        std = sqrt(std)
+        info(f"results for log {test_no+1} of axiom 9 are :: {mean=} | {std=}.")
+        info(f"unique results observed :: {set(results)}.")
+        runtime = sum(runtimes) / len(runtimes)
+        info(f"mean compute time between runs :: {runtime:.1f} secs")
+        mean_computes.append((mean,std))
+        mean_runtimes.append(runtime)
+    info(f"average runtimes for tests : {mean_runtimes}")
+    info("testing completed for axiom nine, to adhere the following series" + 
+        " must contain the same value for each step")
+    info(f"outcome (mean,std) :: {mean_computes}")
+
+if __name__ == "__main__":
+    # guard-recall testing
+    # axiom_3(debug=True)
+    # axiom_4(debug=True)
+    # axiom_6(debug=True)
+    # guard-precision testing
+    axiom_7(debug=True)
+    axiom_8(debug=True)
+    axiom_9(debug=True)
+    pass
